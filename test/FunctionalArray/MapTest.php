eb90be3a4ec5fdd826e4c74e312ad2fc0947be34
Brian Park <re.public@outlook.com>
2017-09-20 22:13:45 -0400

    test: add MapTest

---
+++ test/FunctionalArray/MapTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class MapTest extends \PHPUnit_Framework_TestCase {
+
+    public function testMapNumeric() {
+        $factor = 2;
+        $numbers = (new FunctionalArray(range(1, 10)))
+          ->map    (function($value) use ($factor)  { return $value * $factor;  })
+          ->filter (function($value)                { return $value >= 10;      });
+        $this->assertEquals($numbers->get(), [10, 12, 14, 16, 18, 20]);
+    }
+}
